7f32a229d5b434b0b897c90d5e3d7e135808a959
Sean Lee <xmlee97@gmail.com>
2023-06-22 20:29:07 +0800

    init commit
    update citation
    
    add pretrained model link
    
    add bumpversion

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+readme = open('README.md').read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
+with open('dev-requirements.txt', encoding='utf-8') as f:
+    test_requirements = [l for l in f.read().splitlines() if l][1:]
+
+setup(
+    name='rannet',
+    version='0.0.1',
+    description='Recurrent Attention Networks',
+    long_description=readme,
+    author='sean lee',
+    author_email='xmlee97@gmail.com',
+    packages=find_packages(exclude=("tests", "tests.*", "examples", "examples.*")),
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='rannet',
+    classifiers=[
+        'Private :: Do Not Upload',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+)
